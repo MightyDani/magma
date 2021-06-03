ac37b1bdd7c8c7ff29067908296ce90e9bafda06
Marie Bremner <marwhal@fb.com>
2021-06-03 15:26:39 -0500

    test out change without integ
    Signed-off-by: Marie Bremner <marwhal@fb.com>

--- lte/gateway/fabfile.py
+++ lte/gateway/fabfile.py
@@ -257,8 +257,8 @@ def integ_test(gateway_host=None, test_host=None, trf_host=None,
     else:
         ansible_setup(test_host, "test", "magma_test.yml")
 
-    execute(_make_integ_tests)
-    execute(_run_integ_tests, gateway_ip)
+    #execute(_make_integ_tests)
+    #execute(_run_integ_tests, gateway_ip)
 
     if not gateway_host:
         setup_env_vagrant()
